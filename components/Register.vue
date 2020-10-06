9503988a6e3e2920a0f0ed716546eeae29baa790
jjmm0 <jedrzej.miga@gmail.com>
2020-10-06 18:12:36 +0200

    Register Component (no styles)
    Added register component with basic styles

---
+++ components/Register.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="stuff">
+      <label for="login">Nazwa użytkownika</label>
+      <br>
+      <input id="login">
+      <br>
+      <label for="password">Hasło</label>
+      <br>
+      <input id="password">
+      <br>
+      <label for="confirmPassword">Potwierdź hasło</label>
+      <br>
+      <input id="confirmPassword">
+      <br>
+      <div class="button">
+        <button type="submit" class="submitButton">Zarejestruj się</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  @import '~/assets/style/Register/registerStyles.css';
+</style>

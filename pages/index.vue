3a863b52adbc51273593c06baa63eb518b1f686a
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-10-06 18:45:02 +0200

    Mainsite

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="container">
       <div class="text">
+        <h1>duzy tekst xd</h1><br/>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
       </div>
+      
       <div class="accbuttons">
-        <div class="lbutton">
-          <a class="buttontext">Client</a>
+        <div>
+          <nuxt-link to="/loginPage">
+            <div class="lbutton">
+              <b><a class="buttontext">Client</a></b>
+            </div>
+          </nuxt-link>
         </div>
-        <div class="rbutton">
-          <a class="buttontext">Login/register as a helper</a>
+        
+        <div>
+          <nuxt-link to="/loginPage">
+            <div class="rbutton">
+              <b><a class="buttontext">Login/register as a helper</a></b>
+            </div>
+          </nuxt-link>
         </div>
       </div>
   </div>

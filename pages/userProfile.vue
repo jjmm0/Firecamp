e1760a5127a86d8f8a120a1a15e9d47bdb50e987
jjmm0 <jedrzej.miga@gmail.com>
2020-10-08 21:14:54 +0200

    profile stuff

---
+++ pages/userProfile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <div>
+      <HeaderHelper />
+    </div>
+    <div class="user-pfp">
+      <img src="/" placeholder="Missing pfp" id="userProfilePicture">
+    </div>
+    <div class="userInfo">
+      <div class="infowrap">
+        <div class="userName info">asdasda</div>
+        <div class="userStats info">123</div>
+      </div>
+      
+        <div class="userDesc info"><input type="text" :value="description"></div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+      return{
+        description: 'Description iasvfhba'
+      }
+    }
+}
+</script>
+
+<style scoped>
+@import '~/assets/style/Profile/ProfileStyles.css';
+</style>

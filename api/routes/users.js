24fbf8e6e58c0c3cdeceeb7fc1111fe476c6ba64
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-10-09 23:16:28 +0200

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,9 +4,10 @@ const router = Router()
 const userController = require('../controllers/userController')
 const checkAuth = require('../checkAuth')
 
-
+//Register User
 router.post('/register', userController.register)
 
+//Login User
 router.post('/login', userController.login)
 
 module.exports = router

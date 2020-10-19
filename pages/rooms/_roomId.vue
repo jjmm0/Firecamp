98d4f8845923474c03342303e06152a6b610c336
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-10-19 19:49:33 +0200

    Chat

--- pages/rooms/_roomId.vue
+++ pages/rooms/_roomId.vue
@@ -2,11 +2,13 @@
     <div>
         <div style="background-color: white; width: 1500px; height: 600px; margin: 40px; padding: 1%;">
             <div style="background-color: gray; color: green; height: 100%; width: 100%">
-                <div>Wiadomosc</div>
-                <div>Wiadomosc</div>
-                <div>Wiadomosc</div>
+                <div v-for="msg in messages">
+                    <div>{{msg.msg}}</div>
+                    <div>{{msg.nick}}</div>
+                </div>
+                {{chat.socket}}
             </div>
-            <input style="background-color: red;" @keyup.enter="send(chatInput)" v-model="chatInput" type="text" />
+            <input style="background-color: red;" @keyup.enter="send(chat.input)" v-model="chat.input" type="text" />
         </div>
     </div>
 </template>
@@ -15,12 +17,22 @@
 export default {
     data(){
         return{
-            chatInput: "lol"
+            chat: {
+                socket: this.$route.params.roomId,
+                input: '',
+            },
+            messages: []
         }
+    },
+    mounted(){
+        this.socket = this.$nuxtSocket({})
+        this.socket.on('newMessage', (message) => {
+            this.messages.push({msg: message.input, nick: 'jakisnick'})
+        })
     },
     methods: {
         send(message){
-            alert(message)
+            this.socket.emit('newMessage', this.chat)
         }
     }
 }

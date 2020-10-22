8ce868155ec7bdb3259303542df1bd05dd498a85
jjmm0 <jedrzej.miga@gmail.com>
2020-10-22 23:13:06 +0200

    Buttons styles for chat

--- pages/rooms/_roomId.vue
+++ pages/rooms/_roomId.vue
@@ -20,12 +20,17 @@
              <div class="helperComp">
                 <img class="helperComp" :src="`/api/avatar/${helperID}`" placeholder="Nie ma obrazka" id="userProfilePicture">
              </div>
-             <div class="likeButton button">
-                 <svg class="Like" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path></svg>
-                 Like
+             <div class="likeContainer">
+                <div class="likeButton button">
+                    <svg class="Like" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path></svg>
+                    Like
+                </div>
              </div>
-             <div class="endButton button">
-
+             <div class="endContainer">
+                <div class="endButton button">
+                    <svg class="Exit" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+                    Zakończ czat
+                </div>
              </div>
         </div>
     </div>
@@ -63,7 +68,6 @@ export default {
         // Take helper data
         this.socket.on('helperData', (helperID) => {
             this.helperID = helperID
-            // console.log(this.helperID)
         })
     },
     methods: {
